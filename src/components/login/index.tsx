28d0a4dd81b4a8f5f8cdbe2506726d87fcc979bd
lys <lys219911@gmail.com>
2022-12-06 14:34:09 +0800

    feat: 完成登录功能

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './index.scss';
 
 // 参考 https://codesandbox.io/s/react-demo-11-13-ptfex?file=/src/login.js:1189-1358
 
 function Login() {
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+
+  const userLogin = async () => {
+    const postData = {
+      email: userEmail,
+      password: userPassword,
+    };
+    try {
+      const res = await axios.post('/api/auth/login', postData);
+      console.log(res.data.data.token);
+      axios.defaults.headers.common.Authorization = `Bearer ${res.data.data.token}`;
+      console.log(await axios.get('/api/auth/info'));
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+    }
+  };
+
   return (
     <div className="login">
       <h2>欢迎光临</h2>
       <form className="form">
         <div className="formItem">
-          <label htmlFor="username">
-            用户名：
+          <label htmlFor="eamil">
+            邮箱：
           </label>
-          <input type="text" id="username" />
+          <input
+            type="text"
+            id="eamil"
+            value={userEmail}
+            onChange={(e) => {
+              setUserEmail(e.currentTarget.value);
+            }}
+          />
         </div>
         <div className="formItem">
           <label htmlFor="password">
@@ -21,15 +56,16 @@ function Login() {
           <input
             type="password"
             id="password"
-            onChange={(e) => { }}
+            value={userPassword}
+            onChange={(e) => {
+              setUserPassword(e.currentTarget.value);
+            }}
           />
         </div>
         <div
           role="presentation"
           className="loginBtn"
-          onClick={() => {
-
-          }}
+          onClick={userLogin}
         >
           登录
         </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import axios from 'axios';
 import App from './components/App';
 import { store } from './store';
 import './index.scss';
 
+axios.defaults.baseURL = 'http://localhost:9090';
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
